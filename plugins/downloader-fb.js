c706deaf2a131c93a2b86c13a98ce45367fb9578
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-06-21 07:48:28 -0500

    Update downloader-fb.js

--- plugins/downloader-fb.js
+++ plugins/downloader-fb.js
@@ -1,9 +1,9 @@
 let xfar = require('xfarr-api')
 let fetch = require('node-fetch')
 let handler = async (m, { conn, command, text }) => {
-    if (!text) throw 'Masukkan Link\n\nContoh: .fb https://facebook.com/xxxxxx'
+    if (!text) throw 'Introducir enlace\n\nEjemplo: .fb https://facebook.com/xxxxxx'
   let res = await xfar.Facebook(text)
-m.reply('*Tunggu Sebentar...*')
+m.reply('*Espera un momento...*')
 conn.sendFile(m.chat,res.medias[1].url, 'fb.mp4', `Tittle: ${res.tittle}
 Link Video: ${res.url}
 `, m)
