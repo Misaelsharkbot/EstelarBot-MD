eebe12b73307b7a53d0df518420ce824327a5e9c
Misael <101156132+Misaelsharkbot@users.noreply.github.com>
2022-06-21 07:33:09 -0500

    Update anime-loli.js

--- plugins/anime-loli.js
+++ plugins/anime-loli.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
 let api-hyzer = 'https://api.zacros.my.id/randomimg/loli'
-    conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.loli', m)
+    conn.sendButtonImg(m.chat, api-hyzer, 'Onii-chan', wm2, 'Siguiente ➡', '.loli', m)
 }
 handler.help = ['loli']
 handler.tags = ['anime']
